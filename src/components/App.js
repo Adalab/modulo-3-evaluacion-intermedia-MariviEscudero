5bf7a0ceedb65c4ac8b115e215b1e0871ad57e48
MariviEscudero <mariviesmo@gmail.com>
2021-09-24 08:57:16 +0200

    add folders

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import '../styles/App.scss';
+import dataList from './data.json';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+export default App;
