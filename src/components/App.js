853ebc25c821d37824927df49e5bee75daeed64e
MariviEscudero <mariviesmo@gmail.com>
2021-09-24 10:21:20 +0200

    Publish in Github Pages

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,30 @@ import dataList from './data.json';
 
 const App = () => {
   const [data, setData] = useState(dataList);
+  const [clubName,setClubName] = useState('');
+  const [openWeek, setOpenWeek] = useState(false);
+  const [openWeekend, setOpenWeekend] = useState(false);
+
+  const handleClubName = (ev) =>{
+    setClubName(ev.target.value);
+  }
+  const handleOpenWeek = (ev) =>{
+    setOpenWeek(ev.target.checked);
+  }
+  const handleOpenWeekend = (ev) =>{
+    setOpenWeekend(ev.target.checked);
+  }
+  
 
   const renderClubs = () => {
     return(
-      dataList.map((data,index)=>{
+      data
+      /*.filter(() =>{
+        return(
+
+        )
+      })*/
+      .map((data,index)=>{
         return(
           <li className="main__list--item" key={index}>
             <h2>{`#${index}: ${data.name}`}</h2>
@@ -30,13 +50,13 @@ const App = () => {
         <h3>Añadir un nuevo club</h3>
         <form action="" className="form">
           <label htmlFor="add-club">Nombre del club</label>
-          <input name="add-club" type="text" value="var" />
-          <label for="daily">
-            <input id="" type="checkbox" value="" name="daily" />
+          <input name="add-club" type="text" value={clubName} onChange={handleClubName} />
+          <label htmlFor="daily">
+            <input id="" type="checkbox" name="daily" onChange={handleOpenWeek}/>
             ¿Abre entre semana?
           </label>
-          <label for="weekend">
-            <input id="" type="checkbox" value="" name="weekend" />
+          <label htmlFor="weekend">
+            <input id="" type="checkbox" name="weekend" onChange={handleOpenWeekend}/>
             ¿Abre los fines de semana?
           </label>
           <button>Añadir un nuevo club</button>
